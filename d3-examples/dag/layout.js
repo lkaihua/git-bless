fe7bfef40bb0edffa077765f4cb169141a91560b
Kaihua Liu <lkaihua@gmail.com>
2018-11-13 08:29:31 +0100

    add HEAD

--- d3-examples/dag/layout.js
+++ d3-examples/dag/layout.js
@@ -1,14 +1,15 @@
 'use strict';
 
 function addOnceArrowMarker(svg, id, reversed = true) {
-  const arrow = "M 0,0 0.04,0.02 0,0.04 0.01,0.02";
-  const arrowReversed = "M 0,0.02 0.04,0.04 0.03,0.02 0.04,0"
+  const u = 0.0075;
+  const arrow = `M 0,0 ${4*u},${2*u} 0,${4*u} ${u},${2*u}`;
+  const arrowReversed = `M 0,${2*u} ${4*u},${4*u} ${3*u},${2*u} ${4*u},0`
 
   if (!svg.select(`marker#${id}`).size()) {
     svg.append("defs").append("marker")
       .attr("id", id)
-      .attr("refX", 0.02)
-      .attr("refY", 0.02)
+      .attr("refX", 2*u)
+      .attr("refY", 2*u)
       // .attr("markerWidth", 0.10)
       // .attr("markerHeight", 0.10)
       .attr("markerUnits", "userSpaceOnUse")
@@ -50,6 +51,35 @@ function hash(key) {
   return result;
 }
 
+function updateTagBox(d, radius = 0.05) {
+  const branchBoxes = d.data.branches && d.data.branches.length ? 
+    d.data.branches.map((branch,index) => {
+      let branchType;
+      let y = radius + 0.04 + index * 0.07;
+      switch (branch) {
+        case "master":
+          branchType = "isMaster";
+          break;
+  
+        default:
+          branchType = "isBranch";
+          break;
+      }
+      return `<rect class="${branchType}" rx="0.01" ry="0.01" x="-0.1" y="${y}" width="0.2" height="0.06"></rect>`
+    }) : []; 
+  const headBox = "" // d.data.head ? `<rect class="isHead" rx="0.01" ry="0.01" x="-0.05" y="-0.16" width="0.1" height="0.06"></rect>` : "";
+  return [...branchBoxes, headBox].join("");
+}
+
+function updateTagText(d, radius = 0.05){
+  const branchTexts = d.data.branches && d.data.branches.length ? 
+    d.data.branches.map((branch,index) => {
+      return `<text class="branchText" dy="${radius + 0.02 + index * 0.07 + 0.06}">${branch}</text>`
+    }) : [];
+  const headText = d.data.head ? `<text class="headText" dy="${-radius - 0.02}">HEAD</text>`: ""
+  return [...branchTexts, headText].join("")
+}
+
 function layout(dag, w = 500, h = 500) {
   const padding = {
     top: 0.06,
@@ -79,6 +109,7 @@ function layout(dag, w = 500, h = 500) {
   sortedNodes.sort((a, b) => {
     return a.id - b.id;
   })
+
   const sortedLinks = dag.links();
   sortedLinks.sort((a, b) => {
     const targetOrder = a.target.id - b.target.id;
@@ -88,46 +119,17 @@ function layout(dag, w = 500, h = 500) {
     return targetOrder;
   })
 
-  const tags = svg.select('.tag').size() == 0 ?
-    svg.append('g').classed('tag', true) :
-    svg.select('.tag');
 
-  const allTags = tags.selectAll('g').data(sortedNodes);
-  
-  const filledTags = allTags.enter().filter(d => d.data.branches && d.data.branches.length)
-    .append("g")
-      .attr('transform', ({x, y}) => `translate(${x * ratio}, ${y})`)
-  
-  filledTags.append("g").classed("tag-box", true).html(d => {
-    // const branches = ;
-    const branchBoxes = d.data.branches.map((branch,index) => {
-      let branchType;
-      let y = radius + 0.02 + index * 0.07;
-      switch (branch) {
-        case "master":
-          branchType = "isMaster";
-          break;
-  
-        default:
-          branchType = "isBranch";
-          break;
-      }
-      return `<rect class="${branchType}" rx="0.01" ry="0.01" x="-0.1" y="${y}" width="0.2" height="0.06"></rect>`
-    })
-    const headBox = "" // d.data.head ? `<rect class="isHead" rx="0.01" ry="0.01" x="-0.05" y="-0.16" width="0.1" height="0.06"></rect>` : "";
-    return [...branchBoxes, headBox].join("");
-  })
-
-
-
+  /** 
+   * Links: the directed edges between nodes.
+   *  
+   * Here we draw links in lower canvas layers, so other elements 
+   * will not be covered by links.
+   **/
   const links = svg.select('.link').size() == 0 ?
     svg.append('g').classed('link', true) :
     svg.select('.link');
 
-  // Delay displaying `marker-mid` marker and other data points
-  // to the animation stage for two reasons:
-  // 1. A safari bug. Rendering mid point will overwrite part of circles
-  // 2. The initial animation of arrows looks quite cool :)
   links.selectAll('path')
     .data(sortedLinks).enter().append('path')
     .attr('d', ({source, target, data}) => line(
@@ -140,24 +142,49 @@ function layout(dag, w = 500, h = 500) {
       }])
     ));
 
+  /**
+   * Nodes: each node has a circle, an id inside of it, and a hash text
+   * 
+   */
+
   const nodes = svg.select('.node').size() == 0 ?
     svg.append('g').classed('node', true) :
     svg.select('.node');
 
-  const enterNode = nodes.selectAll('g').data(sortedNodes)
-    .enter()
-    .append('g')
+  // const allNodes = nodes.selectAll('g')
+  const allNodes = nodes.selectAll('g').data(sortedNodes, d => d.id)
+  
+  const enterNode = allNodes.enter().append('g')
 
   enterNode
     .attr('transform', ({x, y}) => `translate(${x * ratio}, ${y})`)
     .on("click", function (d) {
-      console.log(d)
+      // Output the info of this node, including {id, layer, x, y, children, data}
+      // console.log(d)
     })
     .append('circle')
     .attr('r', radius)
-
-
   
+  // enterNode.exit().remove();
+
+  /**
+   * Tags: HEAD tag, branch tag name and background box.
+   */
+  const tags = svg.select('.tag').size() == 0 ?
+    svg.append('g').classed('tag', true) :
+    svg.select('.tag');
+  
+  const allTags = tags.selectAll('g').data(sortedNodes, d => d.id);
+  
+  const filledTags = allTags.enter().filter(d => d.data.branches && d.data.branches.length)
+    .append("g").classed('tag-item', true)
+      .attr('transform', ({x, y}) => `translate(${x * ratio}, ${y})`)
+  
+  filledTags.append("g").classed("tag-box", true).html(d => updateTagBox(d))
+  
+  allTags.selectAll('.tag-box').html(d => updateTagBox(d))
+  
+
     
   /**
    * Measure and trim.
@@ -174,36 +201,20 @@ function layout(dag, w = 500, h = 500) {
     ].join(' ')
   );
 
-  // 
-  // enterNode.selectAll('.branch').data(sortedNodes).enter()
-  //   .append('text').classed('branch', true).text(d => {
-  // filledTags.append('text')
-  //   .classed('branchText', true)
-  //   .text(d => abbreviate(d.data.branches))
-  //   .attr("dy", "0.1")
+  /****** Add text after updating view box ******/
 
+  filledTags.append('g').classed("tag-text", true).html(d => updateTagText(d))  
 
-  filledTags.append('g').classed("tag-text", true).html(d => {
-    const branchTexts = d.data.branches.map((branch,index) => {
-      return `<text class="branchText" dy="${radius + 0.02 + index * 0.07 + 0.04}">${branch}</text>`
-    })
-    const headText = d.data.head ? `<text class="headText" dy="${-radius - 0.065}">HEAD</text>`: ""
-    return [...branchTexts, headText].join("")
-  })  
+  allTags.selectAll('.tag-text').html(d => updateTagText(d))
 
   enterNode.append('text').text(d => d.data.name || d.id)
     .attr("dy", "0.015");
+
   enterNode.append('text').text(d => d.data.hash || hash(d.id))
-    .attr("dy", "-0.065").classed("hashText", true)
+    .attr("dy", "0.08").classed("hashText", true)
 
-  // enterNode.append('rect')
-  //   .attr("width", 0.1)
-  //   .attr("x", -0.05)
-  //   .attr("y", 0)
-  //   .attr("height", 0.1)
-  //   .classed("branch-tag", true)
-
-  // Animations
+  /****** Animations ******/
+  
   links.selectAll('path')
     .transition()
     .duration(duration)
@@ -233,26 +244,10 @@ function layout(dag, w = 500, h = 500) {
     .classed('isBranch', d => !!d.data.branches && d.data.branches.length)
     .classed('isMaster', d => !!d.data.branches && d.data.branches.includes("master"))
     .classed('isHead', d => !!d.data.head)
-
-  filledTags.style("opacity", 0)
-    .transition()
-    .style("opacity", 1)
-    .duration()
-
-  // TODO: here the filter is removing node-2 by mistake
-  allTags.filter(d => {
-    let a = !d.data.branches || !d.data.branches.length
-    console.log(d, a)
-    return a
-  })
-    .transition()
-    .style("opacity", 0)
-    .duration(duration)
-    .remove()
-    
-  // filledTags.selectAll('g')
-  //   .transition()
-  //   .duration(duration)
-  //   .attr('transform', ({x, y}) => `translate(${x * ratio}, ${y})`)
   
+  tags.selectAll('g.tag-item')
+    .transition()
+    .duration(duration)
+    .attr('transform', ({x, y}) => `translate(${x * ratio}, ${y})`)
+
 }
